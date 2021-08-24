255448a0b80155548572dd474da598024b8451fa
Sabin Marcu <sabinmarcu@gmail.com>
2021-08-24 14:18:21 +0200

    fix: next export

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+const rules = {
+  'import/prefer-default-export': 0,
+  'import/no-dynamic-require': 0,
+  'global-require': 0,
+  'react/react-in-jsx-scope': 0,
+  'react/prop-types': 0,
+  'react/jsx-props-no-spreading': 0,
+};
+
+module.exports = {
+  extends: ['eslint-config-airbnb-base'],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  rules,
+  overrides: [
+    {
+      files: ['.eslintrc.js'],
+      rules: {
+        'import/no-extraneous-dependencies': 0,
+      },
+    },
+    {
+      files: ['*.ts', '*.tsx'],
+      extends: ['eslint-config-airbnb-typescript'],
+      rules,
+    },
+  ],
+};

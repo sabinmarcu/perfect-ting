91ef1c89b47a744f7d62c2df2b6f375f20f34ad3
Sabin Marcu <sabinmarcu@gmail.com>
2021-08-24 14:32:27 +0200

    fix(prettier): remove prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,12 +15,8 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing
           <code className={styles.code}>pages/index.js</code>
         </p>
 
@@ -63,7 +59,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by
           <span className={styles.logo}>
             <img src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
